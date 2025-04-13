a7ae62ffcbd2d8fe64ccb377025cc7a00a3f1fd7
Salah S. <salah.s2002@proton.me>
2025-04-12 21:17:01 -0400

    Implemnted code changes.

--- FastFoodAPI/Controllers/WorkstationController.cs
+++ FastFoodAPI/Controllers/WorkstationController.cs
@@ -1,6 +1,65 @@
-namespace FastFoodAPI.Controllers;
+using FastFoodAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-public class WorkstationController
+namespace FastFoodAPI.Controllers
 {
-    
+    public class WorkstationController : Controller
+    {
+        public ILogger<WorkstationController> _logger;
+        private readonly FastFoodDbContext _fastFoodDbContext;
+        public WorkstationController(ILogger<WorkstationController> logger, FastFoodDbContext fastFoodDbContext)
+        {
+            _logger = logger;
+            _fastFoodDbContext = fastFoodDbContext;
+            
+        }
+        
+        /// <summary>
+        /// This method returns all available stations.
+        /// </summary>
+        [HttpGet("employee/workstations")]
+        public IActionResult GetStations()
+        {
+            var stations = _fastFoodDbContext.Stations.Select(station => new
+            {
+                station.StationId,
+                station.StationName
+            }).ToList();
+            return Ok(stations);
+        }
+        
+        /// <summary>
+        /// This method returns all available training modules.
+        /// </summary>
+        [HttpGet("employee/trainingmodules")]
+        public IActionResult GetTrainingModules()
+        {
+            var trainingModules = _fastFoodDbContext.Trainings.Select(training => new
+            {
+                training.TrainingId,
+                training.TrainingName
+            }).ToList();
+           
+            return Ok(trainingModules);
+        }
+        
+        /// <summary>
+        /// Retrieve role for specific employee.
+        /// </summary>
+        [HttpGet("employee/role/{id}")]
+        public IActionResult GetEmployeeRole(int id)
+        {
+            var employeeRole = _fastFoodDbContext.Employees
+                .Where(e => e.EmployeeId == id)
+                .Select(e => new
+                {
+                    e.JobTitleId
+                })
+                .FirstOrDefault();
+            
+            return Ok(employeeRole);
+        }
+        
+    }
 }
+
